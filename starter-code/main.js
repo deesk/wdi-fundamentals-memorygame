c44a3a4a363b729453f171f804e0106506c036a2
deSk <de.sandesh@gmail.com>
2017-01-11 14:15:00 +1100

    cards created by js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,28 @@
-console.log("JS file is connected to HTML! Woo!")
+alert("JS file is connected to HTML! Woo!");
 
-var cardOne= "king";
-var cardTwo= "king";
-var cardThree= "queen";
-var cardFour= "queen";
+// var cardOne= "queen";
+// var cardTwo= "queen";
+// var cardThree= "king";
+// var cardFour= "king";
+
+// if (cardOne === cardTwo) {
+// 	alert('You found a match!');	
+// } else {
+//   alert('Sorry, try again.');
+// }
+
+var board= document.getElementById('game-board');
+var totalCards= 4;
+
+function createCards(){
+	for(var i=0; i < totalCards; i++){
+		var cards= document.createElement('div');
+		cards.className ='card';
+		board.appendChild(cards);
+	}
+
+};
+
+createCards();
+
+
